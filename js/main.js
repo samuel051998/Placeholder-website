e2706eca7e5ef907ce56447bd7f7b2a49e16e862
samuele <samuele@amasoftware.com>
2020-02-19 11:38:40 +0100

    modifiche player yt

--- js/main.js
+++ js/main.js
@@ -4,5 +4,26 @@
     maxOffset: 500
 });*/
 
+
+ // ############### PARALLAX ####################
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
+// ############### PARALLAX ####################
+
+
+// ############### YOUTUBE PLAYER ####################
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytplayer', {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.mute();
+    player.playVideo();
+}
+// ############### YOUTUBE PLAYER ####################
