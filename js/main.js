35aefb9b2cba9aca86ce0b14bc811fe549c46bdd
samuel051998 <samuele.giacconeof@gmail.com>
2020-03-26 18:22:45 +0100

    Completata sezione divario, modifiche sezione gioco

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 // ############### PARALLAX ####################
 
-
 // ############### YOUTUBE PLAYER ####################
 var player;
 
@@ -20,3 +19,31 @@ function onPlayerReady(event) {
     player.playVideo();
 }
 // ############### YOUTUBE PLAYER ####################
+
+// ############### BUTTON ####################
+function clickExtendedText(event) {
+    let text = document.getElementById('ctn1');
+
+    if (text.classList.contains('closed')) {
+        text.classList.remove('closed');
+    } else {
+        text.classList.add('closed');
+    }
+}
+
+function clickExtendedText2(event) {
+    let text = document.getElementById('ctn2');
+
+    if (text.classList.contains('closed')) {
+        text.classList.remove('closed');
+    } else {
+        text.classList.add('closed');
+    }
+}
+
+let firstBtn = document.getElementById('btn1');
+firstBtn.addEventListener('click', clickExtendedText);
+
+let secondBtn = document.getElementById('btn2');
+secondBtn.addEventListener('click', clickExtendedText2);
+// ############### BUTTON ####################
