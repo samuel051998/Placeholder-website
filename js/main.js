b327984eaffc46b02aee8c8255ef5d6396c2cf83
samuel051998 <samuele.giacconeof@gmail.com>
2020-03-30 17:28:36 +0200

    Modifiche slides progetto

--- js/main.js
+++ js/main.js
@@ -69,3 +69,60 @@ let secondBtnx = document.getElementById('btn2x');
 secondBtn.addEventListener('click', clickExtendedText2);
 secondBtnx.addEventListener('click', clickExtendedText2);
 // ############### BUTTON ####################
+
+// ############### CAROUSEL ####################
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName('slide');
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none'; 
+    }
+
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    } 
+
+    x[slideIndex-1].style.display = 'block';
+    setTimeout(carousel, 4000); 
+}
+
+let slideIndex2 = 0;
+carousel2();
+
+function carousel2() {
+    let i;
+    let x = document.getElementsByClassName('slide-image');
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none'; 
+    }
+    slideIndex2++;
+    if (slideIndex2 > x.length) {slideIndex2 = 1;} 
+    x[slideIndex2-1].style.display = 'block'; 
+    setTimeout(carousel2, 4000); 
+}
+// ############### CAROUSEL ####################
+let slideIndex3 = 0;
+carousel3();
+
+function carousel3() {
+    let i;
+    let x = document.getElementsByClassName('changeSlide');
+
+    for (i=0; i < x.length; i++) {
+        x[i].classList.contains('no-active');
+    }
+        
+        slideIndex3++;
+    
+    if (slidex3 > x.length) {
+        slideIndex3 = 1;
+    }
+
+    x[slideIndex3-1].classList.replace('active');
+    setTimeout(carousel3, 4000);
+} 
